125ef4d41f7ca3fa10adeba01dfecddec2e02f5d
Caterina <66692523+Categalafassi@users.noreply.github.com>
2020-11-06 08:38:19 -0300

    lista pasajeros

--- TP_LABO_FINAL/TP_LABO_FINAL/cAvion.h
+++ TP_LABO_FINAL/TP_LABO_FINAL/cAvion.h
@@ -9,10 +9,6 @@ class cAvion
 private:
     cListaT <cPersona> *pasajeros;
     int cant_pasajeros;
-    cListaT <cPersona> *azafatas;
-    int cant_azafatas;
-    cListaT <cPiloto> *pilotos;
-    int cant_pilotos;
 
     int cant_eventos;
     Logger registros;
